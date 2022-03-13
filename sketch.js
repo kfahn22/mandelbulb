7f75eff187c0a2675128199bfcc028067b9d41e7
kfahn22 <65121394+kfahn22@users.noreply.github.com>
2022-03-13 17:11:01 -0400

    Update sketch.js
    add uniforms to sketch

--- sketch.js
+++ sketch.js
@@ -17,12 +17,16 @@ function draw() {
   // send resolution of sketch into shader
   theShader.setUniform('u_resolution', [width, height]);
 
+   // pass uniforms to the shader as named in shadertoy code
+  theShader.setUniform("iResolution", [width, height]);
+  theShader.setUniform("iFrame", frameCount);
+  theShader.setUniform("iMouse", [mouseX, map(mouseY, 0, height, height, 0)]);
+  theShader.setUniform("iTime", [mouseX, millis()]);
+
   // shader() sets the active shader with our shader
   shader(theShader);
 
   // rect gives us some geometry on the screen
   rect(0,0,width, height);
-
-  saveCanvas("mandelbrot");
 }
 
