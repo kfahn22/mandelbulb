99cea0e00cecb130d47ecb728a06d3ae34be8ac2
kfahn22 <65121394+kfahn22@users.noreply.github.com>
2022-03-16 12:30:57 -0400

    Update sketch.js
    Update to include lines of code for different versions of frag file.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,20 @@
+// This code is based on the Mandelbulb Coding Train Challenge by Daniel Shiffman
+// See the frag file for more information
+// This code renders a static image of the mandelbulb using ray marching with phong
+// illumination. If you would like render a version you can move with the mouse change 
+// frag file in loadShader to either movable.frag or regular.frag.
+
 // a shader variable
 let theShader;
 
 function preload(){
   // load the shader
-  theShader = loadShader('basic.vert', 'basic.frag');
+  // this version uses phong illumination to add lighting
+   theShader = loadShader('basic.vert', 'crystal_static.frag');
+  // use this to render a movable version; it takes longer to render
+  // theShader = loadShader('basic.vert', 'crystal_movable.frag');  
+  // this version renders a movable version w/o phong illumination
+  // theShader = loadShader('basic.vert', 'regular.frag'); 
 }
 
 function setup() {
@@ -16,12 +27,8 @@ function setup() {
 function draw() {  
   // send resolution of sketch into shader
   theShader.setUniform('u_resolution', [width, height]);
-
-   // pass uniforms to the shader as named in shadertoy code
-  theShader.setUniform("iResolution", [width, height]);
-  theShader.setUniform("iFrame", frameCount);
   theShader.setUniform("iMouse", [mouseX, map(mouseY, 0, height, height, 0)]);
-  theShader.setUniform("iTime", [mouseX, millis()]);
+  //theShader.setUniform("iTime", [mouseX, millis()]);
 
   // shader() sets the active shader with our shader
   shader(theShader);
